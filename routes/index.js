a7a55769f37950ce3ed05525b04f2346f2924ff5
Your Name <name@example.com>
2017-01-20 20:32:15 +0700

    Register and Login Testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
-
+const models = require('../models')
 /* GET home page. */
+
+// models.User.findById(2).then(function(getData) {
+//     router.get('/', function(req, res, next) {
+//         res.render('index', {
+//             title: getData.name
+//         });
+//     });
+// })
+
+
+router.post('/login', function(req, res, next) {
+    var email = req.body.email
+    var password = req.body.password
+    console.log(email);
+    console.log(password);
+    models.User.findAll().then(function(data) {
+        data.forEach(function(getData) {
+            if (getData.email == email && getData.password == password) {
+                res.redirect('/')
+            } else {
+              console.log("Email Salah");
+            }
+        })
+    })
+})
+
+
+router.post('/register', function(req, res, next) {
+    // create a new user
+    models.User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(function() {
+        console.log("Saving New User");
+        res.redirect('/login')
+    })
+})
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
 });
+
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+router.get('/contact', function(req, res, next) {
+    res.render('contact');
+});
+
+
+
+
+
 
 module.exports = router;
---
+++ seeders/20170120073650-SeedDataUser.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      name: 'kumpulberbagi',
+      email: 'kumpulberbagi@gmail.com',
+      password: "kumpulberbagi",
+      createdAt: new Date(),
+      updatedAt : new Date()
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
